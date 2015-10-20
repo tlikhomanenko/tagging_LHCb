43abcb76bc94770904afdf735539fab7e2b22a8e
Robot Cern-ipcluster <robot-cern-ipcluster@cern-dev-ipython01-iva6.haze.yandex.net>
2015-10-20 20:50:02 +0300

    add docs; cosmetic

--- utils.py
+++ utils.py
@@ -17,17 +17,23 @@ def union(*arrays):
 
 
 def get_events_statistics(data, id_column='event_id'):
+    """
+    :return: dict with 'Events' - number of events and 'tracks' - number of samples
+    """
     return {'Events': len(numpy.unique(data[id_column])), 'tracks': len(data)}
 
 
 def get_events_number(data, id_column='event_id'):
+    """
+    :return: number of B events
+    """
     _, data_ids = numpy.unique(data[id_column], return_inverse=True)
     weights = numpy.bincount(data_ids, weights=data.N_sig_sw) / numpy.bincount(data_ids)
     return numpy.sum(weights)
 
 def get_N_B_events():
     '''
-    return number of B decays (sum of sWeight in initial root file) 
+    :return: number of B decays (sum of sWeight in initial root file) 
     '''
     N_B_decays = 7.42867714256286621e+05
     return N_B_decays
@@ -36,10 +42,13 @@ def get_N_B_events():
 def plot_flattened_probs(probs, labels, weights, label=1, check_input=True):
     """
     Prepares transformation, which turns predicted probabilities to uniform in [0, 1] distribution.
+    
     :param probs: probabilities, numpy.array of shape [n_samples, 2]
     :param labels: numpy.array of shape [n_samples] with labels (0 and 1)
     :param weights: numpy.array of shape [n_samples]
     :param label: int, predictions of this class will be turned to uniform.
+    
+    :return: flattener
     """
     if check_input:
         probs, labels, weights = numpy.array(probs), numpy.array(labels), numpy.array(weights)
@@ -60,15 +69,23 @@ def plot_flattened_probs(probs, labels, weights, label=1, check_input=True):
     return flattener
 
 
-def bootstrap_calibrate_prob(labels, weights, probs, n_calibrations=30, group_column=None):
+def bootstrap_calibrate_prob(labels, weights, probs, n_calibrations=30, group_column=None, threshold=0.):
     """
+    Bootstrap isotonic calibration: 
+     * randomly divide data into train-test
+     * on train isotonic is fitted and applyed to test
+     * on test using calibrated probs p(B+) D2 and auc are calculated 
     
+    :param probs: probabilities, numpy.array of shape [n_samples]
+    :param labels: numpy.array of shape [n_samples] with labels 
+    :param weights: numpy.array of shape [n_samples]
+    :param threshold: float, to set labels 0/1 
+    
+    :return: D2 array and auc array
     """
-    iso_calibs = []
     aucs = []
     D2_array = []
-    omegas = []
-    labels = (labels > 0) * 1
+    labels = (labels > threshold) * 1
     
     for _ in range(n_calibrations):
         if group_column is not None:
@@ -82,22 +99,19 @@ def bootstrap_calibrate_prob(labels, weights, probs, n_calibrations=30, group_co
         probs_calib = iso_est.transform(test_probs)
         alpha = (1 - 2 * probs_calib) ** 2
         aucs.append(roc_auc_score(test_labels, test_probs, sample_weight=test_weights))
-        iso_calibs.append(iso_est)
         D2_array.append(numpy.average(alpha, weights=test_weights))
-        tagging_error_probs = numpy.zeros(len(probs_calib))
-        tagging_error_probs[test_labels == 1] = 1 - probs_calib[test_labels == 1] # 1 - p(B+)
-        tagging_error_probs[test_labels == -1] = probs_calib[test_labels == -1] #1 - p(B-) = p(B+)
-        omegas.append(numpy.average(tagging_error_probs, weights=test_weights))
-    return D2_array, aucs, iso_calibs, omegas
+    return D2_array, aucs
 
 
 def predict_by_estimator(estimator, datasets):
     '''
+    Predict data by classifier
+    Important note: this also works correctly if classifier is FoldingClassifier and one of dataframes is his training data.
+    
     :param estimator: REP classifier, already trained model.
     :param datasets: list of pandas.DataFrames to predict.
         
-    Important note: this also works correctly if classifier is FoldingClassifier and one of dataframes is his training data.
-    :returns: data, probabilities
+    :return: data, probabilities
     '''     
     data = pandas.concat(datasets)    
     # predicting each DataFrame separately to preserve FoldingClassifier
@@ -105,26 +119,24 @@ def predict_by_estimator(estimator, datasets):
     return data, probs
 
 
-def result_table(tagging_efficiency, efficiency_delta, D2, name='model', omega=None):
+def result_table(tagging_efficiency, tagging_efficiency_delta, D2, auc, name='model'):
     """
     Represents results of tagging in a nice table.
     
     :param tagging_efficiency: float, which part of samples will be tagged
-    :param efficiency_delta: standard error of efficiency
-    :param D2: D^2, TODO describe
+    :param tagging_efficiency_delta: standard error of efficiency
+    :param D2: D^2, average value ((p(B+) - 0.5)*2)^2 for sample
     :param name: str, name of model
-    :param omega: \omega, weighted average of (1 - p(B+))
+    :param auc: full auc, calculated with untag events (probs are set 0.5) with B+/B- labels
     
-    :returns: pandas.DataFrame with only one row, describing result_table
+    :return: pandas.DataFrame with only one row, describing result_table
+    
     Use pandas.concat to get table with results of different methods.
     """
     result = OrderedDict()
     result['name'] = name
     result['$\epsilon_{tag}, \%$'] = [tagging_efficiency * 100.]
-    result['$\Delta \epsilon_{tag}, \%$'] = [efficiency_delta * 100.]
-    if omega is not None:
-        result['$w$'] = [numpy.mean(omega)]
-        result['$\Delta w$'] = [numpy.std(omega)]
+    result['$\Delta \epsilon_{tag}, \%$'] = [tagging_efficiency_delta * 100.]
     result['$D^2$'] = [numpy.mean(D2)]
     result['$\Delta D^2$'] = [numpy.std(D2)]
     epsilon = numpy.mean(D2) * tagging_efficiency * 100.
@@ -133,17 +145,23 @@ def result_table(tagging_efficiency, efficiency_delta, D2, name='model', omega=N
     relative_eff_error = efficiency_delta / tagging_efficiency
     relative_epsilon_error = numpy.sqrt(relative_D2_error ** 2 + relative_eff_error ** 2) 
     result['$\Delta \epsilon, \%$'] = [relative_epsilon_error * epsilon]
+    result['AUC, with untag'] = [numpy.mean(auc) * 100]
+    result['$\Delta$ AUC, with untag'] = [numpy.std(auc) * 100]
     return pandas.DataFrame(result)
 
 
 def calibrate_probs(labels, weights, probs, logistic=False, random_state=11, threshold=0.):
-    '''
+    """
     Calibrate output to probabilities using 2-folding to calibrate all data
     
+    :param probs: probabilities, numpy.array of shape [n_samples]
+    :param labels: numpy.array of shape [n_samples] with labels 
+    :param weights: numpy.array of shape [n_samples]
+    :param threshold: float, to set labels 0/1 
     :param logistic: bool, use logistic or isotonic regression
 
     :return: calibrated probabilities
-    '''
+    """
     labels = (labels > threshold) * 1
     ind = numpy.arange(len(probs))
     ind_1, ind_2 = train_test_split(ind, random_state=random_state, train_size=0.5)
@@ -173,6 +191,15 @@ def calibrate_probs(labels, weights, probs, logistic=False, random_state=11, thr
 
 
 def calculate_auc_with_and_without_untag_events(Bsign, Bprobs, Bweights):
+    """
+    Calculate AUC score for data and AUC full score for data and untag data (p(B+) for untag data is set to 0.5)
+    
+    :param Bprobs: p(B+) probabilities, numpy.array of shape [n_samples]
+    :param Bsign: numpy.array of shape [n_samples] with labels {-1, 1}
+    :param Bweights: numpy.array of shape [n_samples]
+    
+    :return: auc, full auc
+    """
     N_B_not_passed = get_N_B_events() - sum(Bweights)
     Bsign_not_passed = [-1, 1]
     Bprobs_not_passed = [0.5] * 2
@@ -186,6 +213,18 @@ def calculate_auc_with_and_without_untag_events(Bsign, Bprobs, Bweights):
 
 def compute_B_prob_using_part_prob(data, probs, weight_column='N_sig_sw', event_id_column='event_id', signB_column='signB',
                                    sign_part_column='signTrack'):
+    """
+    Compute p(B+) using probs for parts of event (tracks/vertices).
+    
+    :param data: pandas.DataFrame, data
+    :param probs: probabilities for parts of events, numpy.array of shape [n_samples]
+    :param weight_column: column for weights in data
+    :param event_id_column: column for event id in data
+    :param signB_column: column for event B sign in data
+    :param sign_part_column: column for part sign in data
+    
+    :return: B sign array, B weight array, B+ prob array, B event id
+    """
     result_event_id, data_ids = numpy.unique(data[event_id_column].values, return_inverse=True)
     log_probs = numpy.log(probs) - numpy.log(1 - probs)
     log_probs *= data[sign_part_column].values
